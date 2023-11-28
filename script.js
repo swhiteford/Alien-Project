58418cee715f5939d7c15abbab899c4997af107b
Susan Whiteford <swhiteford1218@gmail.com>
2023-11-27 19:27:09 -0500

    updated

--- script.js
+++ script.js
@@ -41,9 +41,9 @@ class Ship {
 function AI(name) {
   return new Ship(
     name,
-    (Math.random() * (AI_MAX_HEALTH - 3)) + 3,
-    (Math.random() * 2) + 2,
-    (Math.random() * 0.2) + 0.6,
+    Math.floor(Math.random() *3)+3,
+    Math.floor(Math.random()*3)+2,
+    Math.floor(Math.random() * 0.2) + 0.6,
   );
 }
 
@@ -59,17 +59,20 @@ function Player() {
 
 // Process player attacks
 function playerCanAttack() {
-  return Math.random() >= USSAssembly.accuracy
+  //console.log("I am inside PLAYER ATTACK>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+  return Math.floor(Math.random()*10)+10 >= USSAssembly.accuracy
 };
 
 // Process alien attacks
 function alienCanAttack() {
-  return Math.random() >= Alien.accuracy
-  if (!playerIsDead && playerCanAttack()) {
+  //console.log("I AM HERE IS PLAYERDEAD",playerIsDead);
+  //return Math.random() >= Alien.accuracy
+  if (Alien.hull < USSAssembly.hull) {
     Alien.hull -= USSAssembly.firepower
     alert(`USS Assembly has hit ${Alien.name}!`)
   } else {
-    alert("USS Assembly has missed!")
+    USSAssembly.hull -= Alien.firepower;
+    alert("USS Assembly got hit!")
   }
 
   // If the ship survives, it attacks you
@@ -77,30 +80,31 @@ function alienCanAttack() {
     alert(Alien.name + " is still alive! It now attacks you.")
     USSAssembly.hull -= Alien.firepower;
 
-    if (playerIsDead) {
-        alert("Player has died :(")
+    if (USSAssembly.hull <= 0) {
+        alert("Player has died :( - GAME OVER")
+        attack.disabled = true;
     }
   } else {
     alert(Alien.name + " is dead!")
+    USSAssembly.hull -= Alien.firepower;
     Alien = Aliens.pop()
 
     // Game is over
     if (Aliens.length == 0) {
-      Alien.name = "N/A"
-      Alien.hull = 0
+      //Alien.name = "N/A"
+      //Alien.hull = 0
       alert("You Won!")
     }
+    if (USSAssembly.hull <= 0) {
+      alert("Player has died :( - YOU LOST - Aliens have taken over the earth!!")
+      attack.disabled = true;
+  }
   }
 
   updateWorld();
 };
 
-// coretreat = document.querySelector("#Retreatbutton");
-// retreat.addEventListener("click", function(event) {
-//   if (Aliens.length > 0) {
-//     alert("You lost!  The aliens have taken over earth!")
-//   }
-// })
+
 
 function playerAttack() {
   if (Math.random() >= USSAssembly.accuracy) {
@@ -117,12 +121,23 @@ function alienAttack() {
 
 // Check player's health
 function playerIsDead() {
+  
+  console.log("USS ASSEMBLY HULL:::::: ************%%%%%%%%%%%%%%%%",USSAssembly.hull)
+  
   return USSAssembly.hull <= 0
 };
 
 // Check alien's health
-function alienIsDead(alien) {
-  return alien.hull <= 0
+function alienIsDead(alienhull) {
+  console.log("ALIEN HEALTH ******************************",alienhull)
+  if(alienhull <= 0)
+  {
+      return true;
+  }
+  else{
+    return false;
+  }
+   
 };
 
 //
@@ -161,14 +176,19 @@ updateWorld();
 
 const attack = document.querySelector("#Attackbutton");
 attack.addEventListener("click", function (event) {
+  console.log("US HULL *********************************",USSAssembly.hull)
+  console.log("ALIEN HULL *********************************",Alien.hull);
   // You attack the first alien ship
-  if (!playerIsDead) {
-    playerAttack()
+  if(USSAssembly.hull > Alien.hull) {
+    
+    playerCanAttack();
+    playerAttack();
   }
-
+  console.log("I AM HERE$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$",Alien.hull)
   // If the ship survives, it attacks you
-  if (!alienIsDead) {
+  if (Alien.hull > 0) {
     alert(Alien.name + " is still alive! It now attacks you.")
+    alienCanAttack();
     alienAttack()
   } else {
     alert(Alien.name + " is dead!")
@@ -176,62 +196,64 @@ attack.addEventListener("click", function (event) {
 
     // Game is over
     if (Aliens.length == 0) {
-      Alien.name = "N/A"
-      Alien.hull = 0
+      //Alien.name = "N/A"
+      //Alien.hull = 0
       alert("You Won!")
+      attack.disabled = true;
     }
   }
 
   updateWorld();
 });
 
-retreat = document.querySelector("#Retreatbutton");
-retreat.addEventListener ("click"), function (event) {
-  if (Aliens.length > 0) {
-    alert ("You lost!  The aliens have taken over earth!")
-  }
-}
+// retreat = document.querySelector("#Retreatbutton");
+// retreat.addEventListener ("click"), function (event) {
+//   if (Aliens.length > 0) {
+//     alert ("You lost!  The aliens have taken over earth!")
+//   }
+// }
 
-function playerCanAttack() {
-  return Math.random() >= USSAssembly.accuracy
-};
+// function playerCanAttack() {
+//   return Math.random() >= USSAssembly.accuracy
+// };
 
 // Process alien attacks
-function alienCanAttack() {
-  return Math.random() >= Alien.accuracy
-  if (!playerIsDead && playerCanAttack()) {
-    Alien.hull -= USSAssembly.firepower
-    alert(`USS Assembly has hit ${Alien.name}!`)
-  } else {
-    alert("USS Assembly has missed!")
-  }
+// function alienCanAttack() {
+//   return Math.random() >= Alien.accuracy
+//   if (!playerIsDead && playerCanAttack()) {
+//     Alien.hull -= USSAssembly.firepower
+//     alert(`USS Assembly has hit ${Alien.name}!`)
+//   } else {
+//     alert("USS Assembly has missed!")
+//   }
 
-  // If the ship survives, it attacks you
-  if (!alienIsDead && alienCanAttack()) {
-    alert(Alien.name + " is still alive! It now attacks you.")
-    USSAssembly.hull -= Alien.firepower;
+//   // If the ship survives, it attacks you
+//   if (!alienIsDead && alienCanAttack()) {
+//     alert(Alien.name + " is still alive! It now attacks you.")
+//     USSAssembly.hull -= Alien.firepower;
 
-    if (playerIsDead) {
-        alert("Player has died :(")
-    }
-  } else {
-    alert(Alien.name + " is dead!")
-    Alien = Aliens.pop()
+//     if (playerIsDead) {
+//         alert("Player has died :(")
+//     }
+//   } else {
+//     alert(Alien.name + " is dead!")
+//     Alien = Aliens.pop()
 
-    // Game is over
-    if (Aliens.length == 0) {
-      Alien.name = "N/A"
-      Alien.hull = 0
-      alert("You Won!")
-    }
-  }
+//     // Game is over
+//     if (Aliens.length == 0) {
+//       Alien.name = "N/A"
+//       Alien.hull = 0
+//       alert("You Won!")
+//     }
+//   }
 
-  updateWorld();
-};
+//   updateWorld();
+// };
 
 const retreat = document.querySelector("#Retreatbutton");
 retreat.addEventListener("click", function(event) {
   if (Aliens.length > 0) {
-    alert("You lost!  The aliens have taken over earth!")
+    alert("Game is OVER");
+    window.location.reload();
   }
 })
